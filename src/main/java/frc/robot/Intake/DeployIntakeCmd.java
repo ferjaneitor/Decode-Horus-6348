c952f6b29a5c025678dd1c12dda37f3063974f5b
ferjaneitor <fernandojoel.cruz@gmail.com>
2026-02-17 17:42:13 -0600

    DeployIntakeCmd

--- src/main/java/frc/robot/Intake/DeployIntakeCmd.java
+++ src/main/java/frc/robot/Intake/DeployIntakeCmd.java
@@ -2,10 +2,10 @@ package frc.robot.Intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class DeplyIntakeCmd extends Command {
+public class DeployIntakeCmd extends Command {
   private final IntakeSubsystem m_intakeSubsystem;
 
-  public DeplyIntakeCmd(IntakeSubsystem intakeSubsystem) {
+  public DeployIntakeCmd(IntakeSubsystem intakeSubsystem) {
     m_intakeSubsystem = intakeSubsystem;
     addRequirements(m_intakeSubsystem);
   }
@@ -18,17 +18,19 @@ public class DeplyIntakeCmd extends Command {
   @Override
   public void execute() {
     // Code to keep the intake deployed if necessary
+    m_intakeSubsystem.deployIntake();
   }
 
   @Override
   public void end(boolean interrupted) {
     // Code to stop the intake mechanism if necessary
+    m_intakeSubsystem.stopPivot();
   }
 
   @Override
   public boolean isFinished() {
     // Return true when the intake is fully deployed, or false if it should run indefinitely
-    return true; // Placeholder, change as needed
+    return m_intakeSubsystem.isIntakeDeployed(); // Check if the intake is fully deployed
   }
     
 }
